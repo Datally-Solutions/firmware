3dac237375f8e584d86777695e96f2af2995b412
Mehdi-Leqsiouer <leqsiouer.mehdi@outlook.com>
2026-03-08 12:31:00 +0100

    feat: Add secrets configuration file for network and GCP settings

---
+++ src/secrets.example.h
@@ -0,0 +1,9 @@
+#pragma once
+const char* ssid      = "ssid";
+const char* password  = "password";
+const String botToken = "bot_token";
+const String chatId   = "chat_id";
+const char* sheetsWebhookUrl   = "sheet_webhook_url";
+// GCP Ingest
+const char* ingestUrl   = "cloud_run_url";
+const char* ingestToken = "ingest_token";
