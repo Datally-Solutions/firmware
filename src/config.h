6f0ca66732ceb45c85a9f38d7eab55df77bf35fb
Mehdi-Leqsiouer <leqsiouer.mehdi@outlook.com>
2026-03-10 22:24:32 +0100

    fix: Reset calibration scale value to default

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 #define LOADCELL_SCK_PIN 26
 
 // --- CALIBRATION BALANCE ---
-const float CALIBRATION_SCALE = 27.15f;
+const float CALIBRATION_SCALE = 1.0f;
 
 // --- SEUILS POIDS (kg) ---
 const float SEUIL_ENTREE_KG = 2.0;
