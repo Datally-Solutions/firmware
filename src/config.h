964f875eb767de3cffec184f07fc333b85d25c21
Mehdi-Leqsiouer <leqsiouer.mehdi@outlook.com>
2026-03-10 22:17:26 +0100

    fix: Update calibration scale value to improve accuracy

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 #define LOADCELL_SCK_PIN 26
 
 // --- CALIBRATION BALANCE ---
-const float CALIBRATION_SCALE = 24.79f;
+const float CALIBRATION_SCALE = 27.15f;
 
 // --- SEUILS POIDS (kg) ---
 const float SEUIL_ENTREE_KG = 2.0;
